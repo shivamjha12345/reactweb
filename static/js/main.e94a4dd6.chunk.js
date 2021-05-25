cff414d3d29d1008b05bec829e27c1494125002d
jh <shivam>
2021-05-26 02:46:07 +0530

    Updates

--- static/js/main.e94a4dd6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpapp1=this.webpackJsonpapp1||[]).push([[0],{11:function(n,t,e){"use strict";e.r(t);e(1);var o=e(3),c=e.n(o),i=(e(8),e.p,e(9),e(0));var p=function(n){n&&n instanceof Function&&e.e(3).then(e.bind(null,12)).then((function(t){var e=t.getCLS,o=t.getFID,c=t.getFCP,i=t.getLCP,p=t.getTTFB;e(n),o(n),c(n),i(n),p(n)}))};c.a.render(Object(i.jsx)("h1",{children:"hello world 12345"}),document.getElementById("root")),p()},8:function(n,t,e){},9:function(n,t,e){}},[[11,1,2]]]);
-//# sourceMappingURL=main.e94a4dd6.chunk.js.map
